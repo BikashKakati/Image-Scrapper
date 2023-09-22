8e7a68620f98a057451537d3fbacd74125051ce1
BikashKakati <bikashkakati796@gmail.com>
2023-09-22 11:49:19 +0530

    Done Huge Update
    Updated app.js
    - now it capable to scrap image link, product name, categories
    -now implemented parallel scraping with the help of Promise all

--- app.js
+++ app.js
@@ -1,15 +1,64 @@
-// https://www.amazon.in/HABER-Chevron-Cloev-Collection-Multi-use-Absorbent/dp/${asin}?th=1
-
 const express = require('express');
 const multer = require('multer');
 const ExcelJS = require('exceljs');
 const axios = require('axios');
 const cheerio = require('cheerio');
+const { promisify } = require('util');
+const fs = require('fs');
 
 const app = express();
 const upload = multer({ dest: 'uploads/' });
 
 app.use(express.static('public'));
+
+// Define a function to scrape image links, category hierarchy, and product names for a single ASIN
+const scrapeProductInfo = async (asin) => {
+  try {
+    const productUrl = `https://www.amazon.in/dp/${asin}`;
+    const response = await axios.get(productUrl);
+    const $ = cheerio.load(response.data);
+    
+    // Scrape image link
+    const imageLink = $('#imgTagWrapperId img').attr('src');
+
+    // Scrape category hierarchy
+    const categoryHierarchy = [];
+    $('ul.a-unordered-list li span.a-list-item a.a-link-normal').each((index, element) => {
+      const category = $(element).text().trim();
+      categoryHierarchy.push(category);
+    });
+
+    // Scrape product name
+    const productName = $('#title span#productTitle').text().trim();
+
+    return {
+      asin,
+      imageLink,
+      categoryHierarchy: categoryHierarchy.join(' > ').replace(" > Flash Player",""),
+      productName,
+    };
+  } catch (error) {
+    console.error(`Failed to fetch product details for ASIN ${asin}: ${error.message}`);
+    return null;
+  }
+};
+
+// Define a function to scrape image links, category hierarchy, and product names for a list of ASINs
+const scrapeProductInfoList = async (asinList) => {
+  const productInfoList = [];
+
+  // Implement parallel scraping using Promise.all or other async libraries.
+  // Use a proxy pool if necessary to distribute requests.
+  const scrapeTasks = asinList.map(async (asin) => {
+    const productInfo = await scrapeProductInfo(asin);
+    if (productInfo) {
+      productInfoList.push(productInfo);
+    }
+  });
+
+  await Promise.all(scrapeTasks);
+  return productInfoList;
+};
 
 app.post('/upload', upload.single('file'), async (req, res) => {
   if (!req.file) {
@@ -24,40 +73,45 @@ app.post('/upload', upload.single('file'), async (req, res) => {
     await workbook.xlsx.readFile(inputFile);
     const worksheet = workbook.getWorksheet(1);
     const asinList = [];
+    const asinToRowMap = {};
 
     worksheet.eachRow((row, rowNumber) => {
       if (rowNumber !== 1) {
         const asinCell = row.getCell(1);
         const asin = asinCell.value;
         asinList.push(asin);
+        asinToRowMap[asin] = rowNumber;
       }
     });
 
-    for (let i = 0; i < asinList.length; i++) {
-      const asin = asinList[i];
-      const productUrl = `https://www.amazon.in/HABER-Chevron-Cloev-Collection-Multi-use-Absorbent/dp/${asin}?th=1`;
+    const productInfoList = await scrapeProductInfoList(asinList);
 
-      try {
-        const response = await axios.get(productUrl);
-        const $ = cheerio.load(response.data);
-        const imageLink = $('#imgTagWrapperId img').attr('src');
+    productInfoList.forEach((productInfo) => {
+      const rowNumber = asinToRowMap[productInfo.asin];
+      if (rowNumber) {
+        const cellAddressImage = `B${rowNumber}`;
+        const cellImage = worksheet.getCell(cellAddressImage);
+        cellImage.value = productInfo.imageLink;
 
-        if (imageLink) {
-          const cellAddress = `B${i + 2}`;
-          const cell = worksheet.getCell(cellAddress);
-          cell.value = imageLink;
-        } else {
-          console.warn(`Failed to find image link for ASIN ${asin}`);
-        }
-      } catch (error) {
-        console.error(`Failed to fetch product details for ASIN ${asin}: ${error.message}`);
+        const cellAddressName = `C${rowNumber}`;
+        const cellName = worksheet.getCell(cellAddressName);
+        cellName.value = productInfo.productName;
+
+        const cellAddressCategory = `D${rowNumber}`;
+        const cellCategory = worksheet.getCell(cellAddressCategory);
+        cellCategory.value = productInfo.categoryHierarchy;
+
       }
-    }
+    });
 
-    await workbook.xlsx.writeFile(outputFile);
+    const writeFileAsync = promisify(fs.writeFile);
+    await writeFileAsync(outputFile, await workbook.xlsx.writeBuffer());
 
     res.setHeader('Content-Disposition', 'attachment; filename="output.xlsx"');
     res.sendFile(outputFile, { root: __dirname });
+    
+    // unlink the user file after completing data scraping
+    fs.unlinkSync(inputFile);
   } catch (error) {
     console.error(`Failed to process the uploaded file: ${error.message}`);
     res.status(500).send('Error processing the uploaded file');
